e76ea9498af56c555d1e536a920d92afdb606427
aryankafle <akafl916@gmail.com>
2023-10-12 11:36:47 -0400

    Finished setup for react/node and added heroku dependency

--- Server/index.js
+++ Server/index.js
@@ -6,8 +6,16 @@ const app = express();
 
 
 
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+
+
 app.get("/ping", (req, res) => {
     res.status(200).json({message: "pong"})
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
 
