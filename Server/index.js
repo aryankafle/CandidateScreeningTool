6a8980b8c8939dd6c614eb3e8b36bfa95e061f09
aryankafle <akafl916@gmail.com>
2023-10-12 11:21:45 -0400

    Added react framework to the cstclient folder.

--- Server/index.js
+++ Server/index.js
@@ -4,6 +4,14 @@ require("dotenv").config()
 const PORT = process.env.PORT
 const app = express();
 
+
+
+app.get("/ping", (req, res) => {
+    res.status(200).json({message: "pong"})
+});
+
+
+
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`)
 });
