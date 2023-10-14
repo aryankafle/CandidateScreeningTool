1f297a53ef26c9823a26e9aa73dce98f0bdcd5c6
aryankafle <akafl916@gmail.com>
2023-10-13 23:30:29 -0400

    Removed file serving from backend server

--- Server/index.js
+++ Server/index.js
@@ -5,17 +5,9 @@ const path = require("path")
 const PORT = process.env.PORT || 3001
 const app = express();
 
-app.use(express.static('../cstclient/public'))
-
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../cstclient/public', 'index.html'));
-});
-
 app.get("/ping", (req, res) => {
     res.status(200).json({message: "pong"})
 });
-
-
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`)
