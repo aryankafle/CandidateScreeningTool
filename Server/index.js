bfdeefca6bcabfb594106f235abf0057d5181d01
aryankafle <akafl916@gmail.com>
2023-10-13 20:50:02 -0400

    Node fr goodbye world instead of hello world :(

--- Server/index.js
+++ Server/index.js
@@ -2,21 +2,17 @@ const express = require("express");
 require("dotenv").config()
 const path = require("path")
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001
 const app = express();
 
-
-
-app.use(express.static(path.resolve(__dirname, '../cstclient/public')));
-
-
-
-app.get("/ping", (req, res) => {
-    res.status(200).json({message: "pong"})
-});
+app.use(express.static('../cstclient/public'))
 
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../cstclient/public', 'index.html'));
+});
+
+app.get("/ping", (req, res) => {
+    res.status(200).json({message: "pong"})
 });
 
 
