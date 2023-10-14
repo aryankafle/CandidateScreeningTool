882867af6a035e74efa4fd442ccf3d31702f95cb
aryankafle <akafl916@gmail.com>
2023-10-13 20:09:23 -0400

    Fixed some dependencies and added nodemon

--- Server/index.js
+++ Server/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 require("dotenv").config()
+const path = require("path")
 
 const PORT = process.env.PORT
 const app = express();
 
 
 
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, '../cstclient/public')));
 
 
 
@@ -15,7 +16,7 @@ app.get("/ping", (req, res) => {
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../cstclient/public', 'index.html'));
 });
 
 
