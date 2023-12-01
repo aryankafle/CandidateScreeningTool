41a2100153c4a90d56869ed285d9e4bef6b927c7
martinjang14 <77016729+martinjang14@users.noreply.github.com>
2023-11-30 21:08:48 -0500

    added folder

---
+++ Server/routes/linkedin.js
@@ -0,0 +1,8 @@
+//export default (some object) MAKE SURE TO RETURN SOMETHING, AND THAT THING DOESNT CONTAIN THE RESPONSE VARIABLE
+//AND IT CANT RETURN SOMETHING DIRECTLY FROM THE API, or else it might get messed up if the api changes
+//function that returns a file(json) (facade pattern)
+
+require("dotenv").config()
+const axios = require('axios');
+
+
