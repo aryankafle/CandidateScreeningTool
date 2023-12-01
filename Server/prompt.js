c4c4f4d3d405988819324b8dc7d2ce8b598b0710
aryankafle <akafl916@gmail.com>
2023-11-30 20:58:30 -0500

    installed axios

--- Server/prompt.js
+++ Server/prompt.js
@@ -1,18 +1,20 @@
-
 const OpenAI = require("openai");
 require("dotenv").config()
+
 const client = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
 });
+
 const GPTresponse = client.chat.completions.create({
 
     model: "gpt-3.5-turbo",
     messages: [{role: "user", content: "Say hello"}],
 
 });
-try{
-console.log(GPTresponse.usage.total_tokens);
-console.log(GPTresponse.data.choices[0].message.content);
+
+try {
+  console.log(GPTresponse.usage.total_tokens);
+  console.log(GPTresponse.data.choices[0].message.content);
 }
 catch (err) {
     if (err.response) {
@@ -21,4 +23,4 @@ catch (err) {
     } else {
       console.log(err.message);
     }
-  }
+}
