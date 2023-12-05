2323c89fda32cbf758a95157c4763b32184d5107
aryankafle <akafl916@gmail.com>
2023-12-05 15:35:55 -0500

    forgot this last commit

--- Server/src/models/services/MySQL.service.js
+++ Server/src/models/services/MySQL.service.js
@@ -1,10 +1,6 @@
 import dbConnection from "../../database/mySQL.database.js"
 
 export async function connectDatabase(sql, params) {
-    const connection = await mysql.createConnection(dbConfig.db)
-    connection.connect((err ) => {
-        return err
-    })
 
-    return "Connecte to database"
+    return dbConnection
 }
