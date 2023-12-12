aca26988dd01ab08b9b20eab47b1624f618fc537
aryankafle <akafl916@gmail.com>
2023-12-12 15:38:55 -0500

    added some debug code

--- Server/src/database/mySQL.database.js
+++ Server/src/database/mySQL.database.js
@@ -4,9 +4,9 @@ import mySQLConfig from "../config/mysql.config.js"
 const connection = await mySQL.createConnection(mySQLConfig.db)
 connection.connect(( err ) => {
     if(err) {
-        console.log("MySQL Connection Error: Could not connect to database from server. The following error was thrown: ", err)
+        console.log("MySQL: Connection error: Could not connect to database from server. The following error was thrown: ", err)
     } else {
-        console.log("MySQL succesfully connected to server.")
+        console.log("MYSQL: Succesfully connected to server.")
     }
 })
 
--- Server/src/models/services/MySQL.service.js
+++ Server/src/models/services/MySQL.service.js
@@ -1,6 +1,5 @@
 import dbConnection from "../../database/MySQL.database.js"
 
 export const connectDatabase = async () => {
-    console.log(!!dbConnection)
     return dbConnection
 }
