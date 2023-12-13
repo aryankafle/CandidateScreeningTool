b19956953208391f54e5688932fb4f852de1be2f
chrisjo11 <68395837+chrisjo11@users.noreply.github.com>
2023-12-12 20:06:28 -0500

    example query using database connection

--- Server/src/models/services/MySQL.service.js
+++ Server/src/models/services/MySQL.service.js
@@ -4,3 +4,13 @@ export const connectDatabase = async () => {
     console.log(!!dbConnection)
     return dbConnection
 }
+
+
+// example of query
+// dbConnection.query(
+//     'SELECT * FROM `table` WHERE `name` = "Page" AND `age` > 45',
+//     function(err, results, fields) {
+//       console.log(results); // results contains rows returned by server
+//       console.log(fields); // fields contains extra meta data about results, if available
+//     }
+//   );
