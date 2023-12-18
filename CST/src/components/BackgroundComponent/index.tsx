b4b4520cd3d88d4153ffb8ebf0188a9eb9df7ef3
aryankafle <akafl916@gmail.com>
2023-12-18 15:02:51 -0500

    added background component to layout

---
+++ CST/src/components/BackgroundComponent/index.tsx
@@ -0,0 +1,10 @@
+const BackgroundComponent = () => {
+
+    return (
+        <div className="w-full h-[90px] bg-white dark:bg-gray-800 text-black dark:text-white">
+            Background Component
+        </div>
+    )
+}
+
+export default BackgroundComponent
--- CST/src/router/Layout.route.tsx
+++ CST/src/router/Layout.route.tsx
@@ -1,6 +1,7 @@
 import { Outlet, Link } from "react-router-dom";
 import Footer from '../components/FooterComponent'
 import Header from '../components/HeaderComponent'
+import Background from '../components/BackgroundComponent'
 
 /*
 This component is rendered on all routes.
@@ -11,9 +12,9 @@ const Layout = () => {
     <div>
       <Header />
       <div>
-          <div className="bg-white dark:bg-gray-800 absolute w-full h-[90px] bg-gray-800">
-            
-          </div>
+          <div className="w-full absolute">
+            <Background />
+          </div>  
           <div className="absolute">
             <Outlet />
           </div>
