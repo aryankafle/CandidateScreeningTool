3fd3033d18e1954f86e9ee46044a32bc2f843599
aryankafle <akafl916@gmail.com>
2023-12-18 11:27:14 -0500

    unfixed header

--- CST/src/components/HeaderComponent/index.tsx
+++ CST/src/components/HeaderComponent/index.tsx
@@ -1,7 +1,7 @@
 const HeaderComponent = () => {
 
     return (
-        <header className="fixed top-0 w-full bg-gray-800 text-white">
+        <header className="top-0 w-full bg-gray-800 text-white">
             <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
                 Header Component
             </div>
