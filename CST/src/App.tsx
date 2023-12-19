a91edc9eef59c2abd501962ca280b5fce06a93ba
aryankafle <akafl916@gmail.com>
2023-12-19 09:02:19 -0500

    idk what i did i did this last night probably

--- CST/src/App.tsx
+++ CST/src/App.tsx
@@ -1,13 +1,17 @@
 import { Routes, Route } from "react-router-dom";
-import PageNotFoundPopup from './pages/PageNotFoundScreen';
+import PageNotFoundScreen from './pages/PageNotFoundScreen';
 
-import Home from "./router/Home.route";
-import Auth from "./router/Auth.route";
-import Filter from "./router/Filter.route"
-import Results from "./router/Results.route"
 
+
+import HomeRoutes from "./router/Home.route";
+import AuthRoutes from "./router/Auth.route";
+
+
+
+import FilterScreen from "./pages/FilterScreen"
+import ResultsScreen from "./router/Results.route"
 import SplashScreen from "./pages/SplashScreen"
-import Test from "./test/App.api.test"
+import TestScreen from "./test/App.api.test"
 import BaseLayout from "./router/layouts/BaseLayout.route"
 import DecoratedLayout from "./router/layouts/DecoratedLayout";
 
@@ -15,15 +19,15 @@ function App() {
 return (
       <Routes>
         <Route element={<DecoratedLayout />}>
-            <Route path="/filter" element={<Filter />} />
-            <Route path="/results" element={<Results />} />
+            <Route path="/filter" element={<FilterScreen />} />
+            <Route path="/results" element={<ResultsScreen />} />
         </Route>
         <Route element={<BaseLayout />}>
           <Route index element={<SplashScreen />} />
-          <Route path="/login" element={<Auth />} />
-          <Route path="/home" element={<Home />} />
-        ` <Route path="/test" element={<Test />} />
-          <Route path="*" element={<PageNotFoundPopup />} />
+          <Route path="/login" element={<AuthRoutes />} />
+          <Route path="/home" element={<HomeRoutes />} />
+        ` <Route path="/test" element={<TestScreen />} />
+          <Route path="*" element={<PageNotFoundScreen />} />
         </Route>
       </Routes>
   )
