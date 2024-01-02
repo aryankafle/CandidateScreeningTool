f0e15054945a09e324e3f88c0dc80e8c2be47c3d
aryankafle <akafl916@gmail.com>
2024-01-02 02:43:37 -0500

    fixed spacing issues in homescren

--- CST/src/pages/ResumeUploadScreen/index.tsx
+++ CST/src/pages/ResumeUploadScreen/index.tsx
@@ -12,8 +12,8 @@ const ResumeUploadScreen = () => {
                         w-screen">
             <div className="flex justify-center">
                 <button className="dark:border-white dark:text-white
-                                border-black text-black
-                                border-[1px] flex justify-between w-[10rem]"
+                                    border-black text-black
+                                    border-[1px] flex justify-between gap-[0.5rem] px-[0.7rem]"
                     onClick={handleUploadClick} >
                     <IonIcon className = "pt-[0.3rem]" icon = {cloudUpload}></IonIcon>
                     <div>
