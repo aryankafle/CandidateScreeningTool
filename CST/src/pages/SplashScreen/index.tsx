517a84640be4fcafc26b7e98ea2d853570936edb
aryankafle <akafl916@gmail.com>
2024-01-04 01:39:55 -0500

    winton

--- CST/src/pages/SplashScreen/index.tsx
+++ CST/src/pages/SplashScreen/index.tsx
@@ -2,8 +2,12 @@ const SplashScreen = () => {
 
     return (
         <div>
-            splash screen
+            <span>
+                Splash Screen not designed yet as it is not at all important. Instead of a splash screen here's winston from overwatch.
+            </span>
+            <img src={} alt="winton" />   
         </div>
+             
     )
 }
 
