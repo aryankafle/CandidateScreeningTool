c62830a6240cd9dfdfa06a0cfa2d51148df6cb9f
aryankafle <akafl916@gmail.com>
2024-01-04 06:11:37 -0500

    added cool functionality for select on uplaod screen

--- CST/src/pages/ResumeUploadScreen/index.tsx
+++ CST/src/pages/ResumeUploadScreen/index.tsx
@@ -32,14 +32,30 @@ const ResumeUploadScreen = () => {
     function handleAddFiltersClick() {
         navigate("/filter")
     }
+
+
+
+    function handleRemoveFileFromUpload(file : string) {
+        handleRemoveFileFromSelect(file)
+
+        const temp = uploadedFiles.filter((val) => file !== val)
+        setUploadedFiles(temp)
+    }
+
+
     
     function handleRemoveFileFromSelect(file : string) {
-        const temp = selectedFiles.filter((val) => file != val)
+        const temp = selectedFiles.filter((val) => file !== val)
         setSelectedFiles(temp)
     }
 
     function handleAddFileToSelect(file : string) {
         setSelectedFiles([file])
+        setPreviouslySelected(file)
+    }
+
+    function handleCtrlKeySelect(file : string) {
+        setSelectedFiles([...selectedFiles, file])
         setPreviouslySelected(file)
     }
 
@@ -53,49 +69,67 @@ const ResumeUploadScreen = () => {
         else {
             const uploadIndex = uploadedFiles.indexOf(file)
             const previousUploadIndex = uploadedFiles.indexOf(previouslySelected)
+            
+            var temp;
 
             if(uploadIndex < previousUploadIndex) {
-                const temp = uploadedFiles.slice(uploadIndex, previousUploadIndex < uploadedFiles.length ? previousUploadIndex+1 : undefined)
-                setSelectedFiles(temp)
+                temp = uploadedFiles.slice(uploadIndex, previousUploadIndex < uploadedFiles.length ? previousUploadIndex+1 : undefined)
+                
             } else {
-                const temp = uploadedFiles.slice(previousUploadIndex, uploadIndex < uploadedFiles.length ? uploadIndex+1 : undefined)
-                setSelectedFiles(temp)
+                temp = uploadedFiles.slice(previousUploadIndex, uploadIndex < uploadedFiles.length ? uploadIndex+1 : undefined)
             }
+
+            const combined = [...selectedFiles, ...temp]
+            const uniqueCombined = [...new Set(combined)]
+            setSelectedFiles(uniqueCombined)
         }
+
+        setPreviouslySelected(file)
     }
 
 
 
 
+    const FileCard =  (props: {file: string}) => {
+        return (
+            <div className="flex flex-row justify-between px-[2rem]">
+                {props.file}
+                <div onClick={() => {handleRemoveFileFromUpload(props.file); console.log("")}}>
+                    remove
+                </div>
+            </div>
+        )
+    }
 
-
-    const FileCard = (props: {file: string}) => {
+    const ListCard = (props: {file: string}) => {
         return (
             selectedFiles.includes(props.file) ? 
-                <div className="text-red
-                                    dark:text-red
-                                    text-center">
-                        <button onClick={() => {handleRemoveFileFromSelect(props.file)}}>
-                            <div>
-                                {props.file}
-                            </div>
-                        </button>
+                <div className="text-red border-red
+                                dark:text-red dark:border-red
+                                text-center border-[0.1rem] flex-grow cursor-pointer select-none"
+                        onClick={(e) => {
+                            handleRemoveFileFromSelect(props.file)
+                        }}>
+                    <FileCard file={props.file}/>
                 </div>
+
             :
-                <div className="text-black
-                                    dark:text-white
-                                    text-center">
-                        <button onClick={(e) => {
-                                if(e.shiftKey) {
-                                    handleShiftClickSelect(props.file)
-                                } else {
-                                    handleAddFileToSelect(props.file)
-                                }
-                            }}>
-                            <div>
-                                {props.file}
-                            </div>
-                        </button>
+                <div className="text-black border-black
+                                    dark:text-white dark:border-white
+                                    text-center border-[0.1rem] flex-grow cursor-pointer select-none"
+                        onClick={(e) => {
+                            if(e.shiftKey && !e.ctrlKey) {
+                                handleShiftClickSelect(props.file)
+                            } else if (e.ctrlKey && !e.shiftKey) {
+                                handleCtrlKeySelect(props.file)
+                            } else {
+                                handleAddFileToSelect(props.file)
+                            }
+                        }}
+                        onDragOver={(e) => {
+                            console.log("drag")
+                        }}>
+                        <FileCard file={props.file}/>
                 </div>
         )
         
@@ -119,13 +153,13 @@ const ResumeUploadScreen = () => {
                 <ol className="border-black
                                 dark:border-white
                                 border-[0.1rem] w-[35rem] max-h-[50vh] min-h-[8rem] overflow-y-scroll">
-                    {uploadedFiles.map((file : string, index : number) => <li key={index}><FileCard file={file}></FileCard></li>)}
+                    {uploadedFiles.map((file : string, index : number) => <li key={index}><ListCard file={file}></ListCard></li>)}
                 </ol>
             </div>
             <div className="flex justify-center">
                 <button className="dark:border-white dark:text-white
                                     border-black text-black
-                                    flex justify-center p-[1rem] mb-[1.5rem] mt-[1.5rem] border-[0.1rem]"
+                                    flex justify-center p-[1rem] mb-[4rem] mt-[1.5rem] border-[0.1rem]"
                         onClick={handleAddFiltersClick}>
                     Add Filters to Uploaded Files
                 </button>
