bc2ba1e2a24f7464fdc1798dee3bc907f75421c3
Nathank0126753 <78155248+Nathank0126753@users.noreply.github.com>
2024-01-05 04:43:56 -0500

    component does something

--- CST/src/pages/ViewSavedListsScreen/index.tsx
+++ CST/src/pages/ViewSavedListsScreen/index.tsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { FC, InputHTMLAttributes} from "react";
+import { Key, useContext, useEffect, useState } from "react";
 const ViewSavedListsScreen = () => {
+    
 
-
-    function LinkTextBox(){
-            <div className="w-72">
-                <input> label="Username" </input> 
+    // function LinkTextBox(){
+    //     <input
+    //     type="text"
+    //     value={this.state.value}
+    //     onChange={this.handleChange}
+    //  />
+    // }
+    const LinkBox = (InputProps: {className: string, label: string}) => {
+        return (
+            <div className = "input-wrapper">
+                <label htmlFor = {InputProps.className}>{InputProps.label}</label>
+                <input id={InputProps.className} ></input>
             </div>
+        )
     }
+    
     return(
         <div>
-            grr
+            <LinkBox className = "gruh" label = "buh"></LinkBox>
         </div>
     )
     
