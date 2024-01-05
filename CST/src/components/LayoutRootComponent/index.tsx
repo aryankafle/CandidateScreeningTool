5d09b3872bf24e761c91f115e9c22fb15026f013
aryankafle <akafl916@gmail.com>
2024-01-05 15:09:45 -0500

    fixed bug that causes website to scroll when viewport too small

--- CST/src/components/LayoutRootComponent/index.tsx
+++ CST/src/components/LayoutRootComponent/index.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 const Root = (props: {children : ReactNode}) => {
   return (
-    <div className="w-screen h-screen flex flex-col justify-between">
+    <div className="w-screen h-screen flex flex-col justify-between overflow-clip">
        {props.children}
     </div>
   )
