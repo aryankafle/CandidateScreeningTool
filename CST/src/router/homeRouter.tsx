298144f988670957467d566a3bd6801654b52ef9
chrisjo11 <68395837+chrisjo11@users.noreply.github.com>
2024-01-05 09:43:02 -0500

    fixed naming mistake regarding last commit

--- CST/src/router/homeRouter.tsx
+++ CST/src/router/homeRouter.tsx
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import ViewSavedLists from "../pages/ViewSavedListsScreen"
 import ResumeUpload from "../pages/ResumeUploadScreen"
 import PageNotFoundScreen from "../pages/PageNotFoundScreen"
-import FilteredResultsScreen from "../pages/ResultsScreen"
+import ResultsScreen from "../pages/ResultsScreen"
 
 const HomeRoutes = () => {
     return (
@@ -10,7 +10,7 @@ const HomeRoutes = () => {
             <Routes>
                 <Route path="/resume-upload" element={<ResumeUpload />} />
                 <Route path="/saved-lists" element={<ViewSavedLists />} />
-                <Route path="/results" element={<FilteredResultsScreen />} />
+                <Route path="/results" element={<ResultsScreen />} />
                 <Route path="/" element={<ResumeUpload />} />
                 <Route path="/*" element={<PageNotFoundScreen />} />
             </Routes>
