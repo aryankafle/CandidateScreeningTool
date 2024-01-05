56cac150988a81dcc328c337ed0367465b27c8e9
Nathank0126753 <78155248+Nathank0126753@users.noreply.github.com>
2024-01-05 05:06:34 -0500

    text box

--- CST/src/pages/ViewSavedListsScreen/index.tsx
+++ CST/src/pages/ViewSavedListsScreen/index.tsx
@@ -1,28 +1,54 @@
 import React, { FC, InputHTMLAttributes} from "react";
 import { Key, useContext, useEffect, useState } from "react";
+
+
 const ViewSavedListsScreen = () => {
     
 
-    // function LinkTextBox(){
-    //     <input
-    //     type="text"
-    //     value={this.state.value}
-    //     onChange={this.handleChange}
-    //  />
+    // const LinkBox = (InputProps: {className: string, label: string}) => {
+    //     return (
+    //         <div className = "input-wrapper">
+    //             <label htmlFor = {InputProps.className}>{InputProps.label}</label>
+    //             <input id={InputProps.className} ></input>
+    //         </div>
+    //     )
     // }
-    const LinkBox = (InputProps: {className: string, label: string}) => {
+    type Props = {
+        onChange: (str: string) => void;
+        placeholder: string;
+        name: string;
+        value?: string;
+      };
+      function Input({ onChange, name, placeholder, value = "" }: Props) {
         return (
-            <div className = "input-wrapper">
-                <label htmlFor = {InputProps.className}>{InputProps.label}</label>
-                <input id={InputProps.className} ></input>
-            </div>
-        )
-    }
-    
+          <input
+            onChange={event => onChange(event.target.value)}
+            name={name}
+            placeholder={placeholder}
+            value={value}
+          />
+        );
+      }
+    const [nameInput, setNameInput] = useState("");
+    const onChange = (str: string) => {
+        setNameInput(str);
+    };
+
+
     return(
-        <div>
-            <LinkBox className = "gruh" label = "buh"></LinkBox>
-        </div>
+        //<div>
+            <form>
+                <Input
+                    onChange={onChange}
+                    name="name"
+                    placeholder="Enter your name"
+                    value={nameInput}
+                />
+            </form>
+        //</div>
+        // <div>
+        //     <LinkBox className = "gruh" label = "buh"></LinkBox>
+        // </div>
     )
     
     
