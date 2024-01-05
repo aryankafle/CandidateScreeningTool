b44ff12ebb66d59a01da264f5d6e82758bd7eeef
aryankafle <akafl916@gmail.com>
2024-01-05 14:58:26 -0500

    spacing changes in router index

--- CST/src/router/index.tsx
+++ CST/src/router/index.tsx
@@ -32,7 +32,7 @@ function Router() {
             <Route element={<BaseLayout />}>
                 <Route index element={<SplashScreen />} />
                 <Route path="/login" element={<AuthRoutes />} />
-            ` <Route path="/test" element={<TestScreen />} />
+            `   <Route path="/test" element={<TestScreen />} />
                 <Route path="*" element={<PageNotFoundScreen />} />
             </Route>
             <Route element={<HomeLayout />}>
