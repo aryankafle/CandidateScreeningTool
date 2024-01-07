ce8781c91f7cd46187e0c8968b4324f3fa5288d7
aryankafle <akafl916@gmail.com>
2024-01-07 06:25:16 -0500

    cleanedup file context

--- CST/src/context/FileContext.tsx
+++ CST/src/context/FileContext.tsx
@@ -6,16 +6,12 @@ import { ReactNode, createContext, useState } from "react"
 
 type FileContextType = {
     uploadedFiles: File[]
-    chosenFiles: File[]
     setUploadedFiles: React.Dispatch<React.SetStateAction<File[]>>
-    setChosenFiles: React.Dispatch<React.SetStateAction<File[]>>
 }
 
 const FileContextInitial = {
     uploadedFiles: [] as File[],
-    chosenFiles: [] as File[],
     setUploadedFiles: {} as React.Dispatch<React.SetStateAction<File[]>>,
-    setChosenFiles: {} as React.Dispatch<React.SetStateAction<File[]>>
 }
 
 
@@ -27,10 +23,9 @@ export const FileContext = createContext<FileContextType>(FileContextInitial)
 const FileContextProvider = (props: { children : ReactNode }) => {
 
     const [uploadedFiles, setUploadedFiles] = useState([] as File[])
-    const [chosenFiles, setChosenFiles] = useState([] as File[])
     
     return (
-        <FileContext.Provider value={{uploadedFiles, chosenFiles, setUploadedFiles, setChosenFiles}}>
+        <FileContext.Provider value={{uploadedFiles, setUploadedFiles}}>
             {props.children}
         </FileContext.Provider>
     )
