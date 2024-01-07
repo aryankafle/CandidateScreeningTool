a6443e8a7a540fccfb1101786d526c498c42447f
aryankafle <akafl916@gmail.com>
2024-01-07 12:37:31 -0500

    fixed code for document view component

--- CST/src/components/DocumentView/index.tsx
+++ CST/src/components/DocumentView/index.tsx
@@ -1,21 +1,42 @@
-import React from "react"
-import DocViewer, { DocViewerRenderers } from "react-doc-viewer"
+import React, { Component } from "react"
+import DocViewer, { DocViewerRenderers } from "@cyntler/react-doc-viewer"
 
 type DocumentViewProps = {
-    selectedDocs : File[]
+    files : File[]
+    index: number
 }
 
-export const DocumentView : React.FC<DocumentViewProps> = (props : DocumentViewProps) => {
-    
-    return (
-        <DocViewer
-            documents={
-                props.selectedDocs.map((file) => ({
-                    uri: window.URL.createObjectURL(file),
-                    fileName: file.name,
-                }))
-            }
-            pluginRenderers={DocViewerRenderers}
-        />
-    )
+type DocumentViewState = {
+    files: File[]
 }
+
+class DocumentView extends Component<DocumentViewProps, DocumentViewState> {
+    public readonly state : DocumentViewState = {
+        files: []
+    }
+
+    
+
+    componentDidUpdate(prevProps : DocumentViewProps) {
+        if(prevProps.files !== this.props.files) {
+          this.setState({files: this.props.files});
+        }
+    }
+
+    render() {
+        const selectedDocs = this.state.files.map((file) => ({
+            uri: window.URL.createObjectURL(file),
+            fileName: file.name,
+        }))
+
+        return (    
+            <DocViewer
+                documents={ selectedDocs }
+                initialActiveDocument={ selectedDocs[this.props.index] }
+                pluginRenderers={ DocViewerRenderers }
+            />
+        )
+    }
+}
+
+export default DocumentView
