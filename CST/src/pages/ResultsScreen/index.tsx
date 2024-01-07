9be772958f2e284a7f59fa94ef4f30f85ac3b0de
martinjang14 <77016729+martinjang14@users.noreply.github.com>
2024-01-07 16:53:36 -0500

    add a and b images as well as slight change to results page

--- CST/src/pages/ResultsScreen/index.tsx
+++ CST/src/pages/ResultsScreen/index.tsx
@@ -1,7 +1,13 @@
+import { isNamedTupleMember } from "typescript";
+
 const ResultsScreen = () => {
+
+    const candidates = []; //temporary placeholder for the filtered candidates 
+
     return(
         <div>
-            hello
+            <h1>Here are some great candidates based on your needs:</h1>
+            
         </div>
     );
 }
