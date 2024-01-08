000a144e82eb6349861319883fd56a81179b221c
aryankafle <akafl916@gmail.com>
2024-01-08 16:51:28 -0500

    cleaned up modal code for future use

--- CST/src/components/Modal/index.tsx
+++ CST/src/components/Modal/index.tsx
@@ -1,58 +1,77 @@
 import React, { ReactNode, Component } from "react";
 import Portal from "../Portal";
+import { toggle } from "ionicons/icons";
 
 type ModalProps = {
-    handleClose : () => void
+    onClose : () => void
     className? : string
+    modalTrigger : boolean
 }
 
 type ModalState = {
-    handleClose : () => void
+    onClose : () => void
     className? : string
+    modalTrigger : boolean
 }
 
 class Modal extends Component<ModalProps & { children?: ReactNode }> {
-        
+    
+    private handleClose = () => {
+        this.state.onClose()
+    }
+    
     public readonly state : ModalState = {
-        handleClose: this.props.handleClose,
-        className: this.props.className
+        onClose: this.props.onClose,
+        className: this.props.className,
+        modalTrigger: this.props.modalTrigger
     }
 
     
 
     componentDidUpdate(prevProps : ModalState) {
-        if(prevProps.className !== this.props.className) {
-          this.setState({isOpen: this.props.handleClose, className: this.props.className});
+        if(prevProps != this.props) {
+            this.setState(
+                {
+                    onClose: this.props.onClose,
+                    className: this.props.className,
+                    modalTrigger: this.props.modalTrigger
+                }
+            )
         }
     }
 
 
 
     render() {
-        return <Portal>
-                <div style={{
-                    position: "fixed",
-                    top: 0,
-                    left: 0,
-                    zIndex: 10,
-                    width: "100vw",
-                    height: "100vh",
-                    backgroundColor: "#000000aa",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center"
-                }}>
-                    <div className={"flex flex-col justify-center flex-grow " + this.state.className}>
-                        <div className="self-end" onClick={this.state.handleClose}>
-                            X
-                        </div>
+        return this.state.modalTrigger && <Portal>
+                <div className={"" + this.props.className}
+                    onClick={(event) => {
+                            if(event.currentTarget === event.target) {
+                                this.handleClose();
+                            }
+                        }
+                    }
+                    style={
+                    {
+                        position: "fixed",
+                        top: 0,
+                        left: 0,
+                        zIndex: 10,
+                        width: "100vw",
+                        height: "100vh",
+                        backgroundColor: "#000000aa",
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center"
+                    }
+                }>
+                    <div className="flex flex-grow w-screen h-screen">
                         {this.props.children}
                     </div>
-                    
                 </div>
         </Portal>
     }
-    
+
 }
 
 
