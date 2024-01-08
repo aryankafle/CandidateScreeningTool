4f4712fa9ffe228a07dfec60c9e717b0fdce8ffc
aryankafle <akafl916@gmail.com>
2024-01-08 16:51:46 -0500

    added functional component for popup on resume upload

---
+++ CST/src/components/ViewFilePopup/index.tsx
@@ -0,0 +1,27 @@
+import DocumentView from "../DocumentView";
+import Button from "../ImprovedButtonComponent"
+import { IonIcon } from "@ionic/react"
+import { backspaceOutline } from 'ionicons/icons';
+
+type ViewFileProps = {
+    uploadedFiles : File[]
+    currentlySelectedIndex : number
+    onXClicked : () => void
+}
+
+const ViewFilePopup : React.FC<ViewFileProps> = ({ uploadedFiles, currentlySelectedIndex, onXClicked}) => {
+    return (
+        <div className="bg-white dark:bg-blue
+                        flex flex-col flex-grow m-[10vw]">
+            <Button className="self-end py-[0.3rem] px-[0.6rem]" onClick={onXClicked}>
+                <IonIcon color="white" icon={backspaceOutline}></IonIcon>
+            </Button>
+            <div className="flex flex-col flex-grow overflow-y-clip">
+                <DocumentView files={uploadedFiles} index={currentlySelectedIndex}/>
+            </div>
+        </div>
+    )
+}
+
+
+export default ViewFilePopup
