9d008225c65a2eb18234960eba0c7d696f805eb7
Nathank0126753 <78155248+Nathank0126753@users.noreply.github.com>
2024-01-09 04:27:50 -0500

    had a stroke and finished textbox component

--- CST/src/pages/ViewSavedListsScreen/index.tsx
+++ CST/src/pages/ViewSavedListsScreen/index.tsx
@@ -1,6 +1,6 @@
 import React, { FC, InputHTMLAttributes} from "react";
 import { Key, useContext, useEffect, useState } from "react";
-
+import Input from '../../components/TextBox'
 
 const ViewSavedListsScreen = () => {
     const [nameInput, setNameInput] = useState("");
@@ -8,43 +8,6 @@ const ViewSavedListsScreen = () => {
         setNameInput(str);
     };
 
-    // const LinkBox = (InputProps: {className: string, label: string}) => {
-    //     return (
-    //         <div className = "input-wrapper">
-    //             <label htmlFor = {InputProps.className}>{InputProps.label}</label>
-    //             <input id={InputProps.className} ></input>
-    //         </div>
-    //     )
-    // }
-    type Props = {
-        onChange: (str: string) => void;
-        placeholder: string;
-        name: string;
-        value?: string;
-       // onkeypress: (event: React.KeyboardEventHandler<HTMLElement>) => void;
-    };
-    function Input({ onChange, name, placeholder, value = "" }: Props) {
-        return (
-            <input
-            onChange={event => onChange(event.target.value)}
-            name={name}
-            placeholder={placeholder}
-            value={value}
-            />
-        );
-    }
-    // function handleEnterKey(e){ 
-    //     if(e.keyCode == 13){ // enter pressed
-    //         try{
-    //             e.preventDefault ? e.preventDefault() : (e.returnValue = false);
-    
-    //             //DO ALTERNATE ACTION RATHER THAN SEND ENTER
-    
-    //         }catch(err){
-    //             console.log(err.message); 
-    //         }
-    //     }
-    // }
 
     return(
         //<div>
