e0d217dc779890b97c3c9d9c58be337864507242
martinjang14 <77016729+martinjang14@users.noreply.github.com>
2024-01-08 22:03:52 -0500

    added some random candidates for dummy data

--- CST/src/pages/ResultsScreen/index.tsx
+++ CST/src/pages/ResultsScreen/index.tsx
@@ -2,7 +2,28 @@ import { isNamedTupleMember } from "typescript";
 
 const ResultsScreen = () => {
 
-    const candidates = []; //temporary placeholder for the filtered candidates 
+    class Candidate
+    {
+        name: string;
+        rank: string;
+        constructor(name: string, rank: string){
+            this.name = name;
+            this.rank = rank;
+        }
+    }
+    var candidates = []; //temporary placeholder for the filtered candidates 
+    generateCandidates();
+    
+    function generateCandidates(){
+        for (let i = 0; i < 10; i++){
+            var r = 'A';
+            if (i%2 == 0){
+                r = 'B';
+            }
+            candidates.push(new Candidate('Bob' + i, r))
+        }
+    }
+
 
     return(
         <div className="dark:bg-blue bg-white w-screen">
