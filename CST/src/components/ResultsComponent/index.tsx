3df93949712e07b9e86e210f8aad775762c4d9b0
martinjang14 <77016729+martinjang14@users.noreply.github.com>
2024-01-09 09:19:08 -0500

    moved candidate class from page to component

--- CST/src/components/ResultsComponent/index.tsx
+++ CST/src/components/ResultsComponent/index.tsx
@@ -1,8 +1,36 @@
 
+
 const ResultsComponent = ()=> {
+    class Candidate
+    {
+        name: string;
+        rank: string;
+        constructor(name: string, rank: string){
+            this.name = name;
+            this.rank = rank;
+        }
+
+        get getRank(){
+            return this.rank;
+        }
+        
+    }
+    var candidates = []; //temporary placeholder for the filtered candidates 
+    generateCandidates();
+    
+    function generateCandidates(){
+        for (let i = 0; i < 10; i++){
+            var r = 'A';
+            if (i%2 === 0){
+                r = 'B';
+            }
+            candidates.push(new Candidate('Bob' + i, r))
+        }
+    }
+
     return(
         <div>
-
+            
         </div>
     );
 }
--- CST/src/pages/ResultsScreen/index.tsx
+++ CST/src/pages/ResultsScreen/index.tsx
@@ -1,33 +1,8 @@
+import ResultsComponent from "../../components/ResultsComponent";
+
 const ResultsScreen = () => {
 
-    class Candidate
-    {
-        name: string;
-        rank: string;
-        constructor(name: string, rank: string){
-            this.name = name;
-            this.rank = rank;
-        }
-
-        get getRank(){
-            return this.rank;
-        }
-        
-    }
-    var candidates = []; //temporary placeholder for the filtered candidates 
-    generateCandidates();
     
-    function generateCandidates(){
-        for (let i = 0; i < 10; i++){
-            var r = 'A';
-            if (i%2 === 0){
-                r = 'B';
-            }
-            candidates.push(new Candidate('Bob' + i, r))
-        }
-    }
-
-
     return(
         <div className="dark:bg-blue bg-white w-screen">
             <div className="my-10 max-w-screen-sm p-6 bg-white rounded-r-full flex basis-1/2 place-content-center">
