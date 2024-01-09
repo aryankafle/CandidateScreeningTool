4ca5a6f33c30ed7b7d17c927b7c7597796b96f22
martinjang14 <77016729+martinjang14@users.noreply.github.com>
2024-01-09 09:43:53 -0500

    reverted old changes back

--- CST/src/components/ResultsComponent/index.tsx
+++ CST/src/components/ResultsComponent/index.tsx
@@ -1,36 +1,12 @@
-
+import React from "react";
 
 const ResultsComponent = ()=> {
-    class Candidate
-    {
-        name: string;
-        rank: string;
-        constructor(name: string, rank: string){
-            this.name = name;
-            this.rank = rank;
-        }
-
-        get getRank(){
-            return this.rank;
-        }
-        
-    }
-    var candidates = []; //temporary placeholder for the filtered candidates 
-    generateCandidates();
-    
-    function generateCandidates(){
-        for (let i = 0; i < 10; i++){
-            var r = 'A';
-            if (i%2 === 0){
-                r = 'B';
-            }
-            candidates.push(new Candidate('Bob' + i, r))
-        }
-    }
 
     return(
         <div>
             
+
+
         </div>
     );
 }
--- CST/src/pages/ResultsScreen/index.tsx
+++ CST/src/pages/ResultsScreen/index.tsx
@@ -1,7 +1,38 @@
 import ResultsComponent from "../../components/ResultsComponent";
 
 const ResultsScreen = () => {
+    class Candidate
+    {
+        name: string;
+        rank: string;
+        constructor(name: string, rank: string){
+            this.name = name;
+            this.rank = rank;
+        }
 
+        get getName(){
+            return this.name;
+        }
+
+        get getRank(){
+            return this.rank;
+        }
+        
+    }
+    var candidates: Candidate[] = []; //temporary placeholder for the filtered candidates 
+    generateCandidates();
+    
+    function generateCandidates(){
+        for (let i = 0; i < 10; i++){
+            var r = 'A';
+            if (i%2 === 0){
+                r = 'B';
+            }
+            candidates.push(new Candidate('Bob' + i, r))
+        }
+    }
+
+    //const mappedCandidates = candidates.map((candidates)=><li>{candidates}</li>)
     
     return(
         <div className="dark:bg-blue bg-white w-screen">
