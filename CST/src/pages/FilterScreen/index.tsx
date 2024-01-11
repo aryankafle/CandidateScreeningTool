820f933f430c1dd88a0c454d3e5c3569427b9769
aryankafle <akafl916@gmail.com>
2024-01-11 10:39:31 -0500

    added boxes to filter-page

--- CST/src/pages/FilterScreen/index.tsx
+++ CST/src/pages/FilterScreen/index.tsx
@@ -14,9 +14,29 @@ const FilterScreen = () => {
 
 
 
+    const FilterLayerColumn = () => {
+
+        return <div className="w-[40vw] max-w-[40rem] border-[5px]">
+           asdf 
+        </div>
+    }
+
+    const AddFiltersColumn = () => {
+
+        return <div className="flex-grow border-[5px]">
+            asdf
+        </div>
+    }
+
+
+
     return (
-        <div className="w-screen">
+        <div className="w-screen flex flex-col min-h-[20rem] overflow-auto">
             <HeaderButtons />
+            <div className="justify-between gap-[1rem] flex-grow flex flex-row">
+                <FilterLayerColumn />
+                <AddFiltersColumn />
+            </div>
         </div>
     )
 
