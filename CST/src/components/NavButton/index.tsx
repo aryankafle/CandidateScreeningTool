44b7bb2a66c91b6ebe282bdd350bb0346268bd3c
aryankafle <akafl916@gmail.com>
2024-01-11 10:11:35 -0500

    added navbutton component

---
+++ CST/src/components/NavButton/index.tsx
@@ -0,0 +1,37 @@
+import Button from "../ImprovedButtonComponent"
+import { useNavigate } from "react-router-dom";
+import { ReactNode } from "react";
+
+
+
+
+
+type NavButtonProps = {
+    toRoute : string
+    children : ReactNode
+    className : string
+}
+
+
+
+
+const NavButton = (props: NavButtonProps) => {
+
+    const navigate = useNavigate()
+
+
+
+    return (
+        <Button 
+            className={`${props.className} rounded-md justify-center gap-[0.5rem] border-[0.1rem] flex p-[0.5rem]`}
+            onClick={() => {
+                navigate(props.toRoute)
+            }}
+        >
+            {props.children}
+        </Button>
+    );
+
+}
+
+export default NavButton
