d7bb49f6727dd614b7ea1838654139e70b05d94a
aryankafle <akafl916@gmail.com>
2024-01-11 10:25:18 -0500

    spacing in filter screen

--- CST/src/pages/FilterScreen/index.tsx
+++ CST/src/pages/FilterScreen/index.tsx
@@ -2,16 +2,25 @@ import { useContext } from "react"
 import { FileContext } from "../../context/FileContext"
 import HeaderButtons from "../../components/FilterScreenHeaderButtons"
 
+
+
+
+
 const FilterScreen = () => {
+
     const fileContext = useContext(FileContext)
+
+
+
+
 
     return (
         <div className="w-screen">
             <HeaderButtons />
         </div>
-        
-        
-        
     )
+
 }
+
+
 export default FilterScreen
