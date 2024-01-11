0d485d36f47c6928506934f5858c626e66e7e4fd
aryankafle <akafl916@gmail.com>
2024-01-11 10:24:23 -0500

    added filter screen header buttons component

---
+++ CST/src/components/FilterScreenHeaderButtons/index.tsx
@@ -0,0 +1,38 @@
+import BackButton from "../BackButton"
+import { IonIcon } from "@ionic/react"
+import { colorWandOutline } from "ionicons/icons"
+import NavButton from "../NavButton"
+
+
+
+
+
+const HeaderButtons = () => {
+
+    return (
+        <div className="overflow-auto p-[0.3rem] flex flex-row justify-between">
+            <div className="">
+                <BackButton toRoute="/home"></BackButton>
+            </div>
+            <div>
+                <NavButton
+                        className=" dark:border-white dark:text-white dark:bg-black dark:hover:bg-gray dark:active:bg-blue
+                                    border-black text-black bg-white hover:bg-gray active:bg-blue"
+                        toRoute="/results"
+                >
+                    <IonIcon
+                        icon={colorWandOutline}
+                        size="small"
+                        className="self-center"
+                    />
+                    <span>Apply Filters</span>
+                </NavButton>
+            </div>
+        </div>
+    )
+
+}
+
+
+
+export default HeaderButtons
