13ba59f9ee04955aee083035c0aa33c30a16f4bf
aryankafle <akafl916@gmail.com>
2024-01-11 10:12:11 -0500

    added backbutton and navbutton to filter screen

--- CST/src/pages/FilterScreen/index.tsx
+++ CST/src/pages/FilterScreen/index.tsx
@@ -1,13 +1,37 @@
 import { useContext } from "react"
 import { FileContext } from "../../context/FileContext"
+import BackButton from "../../components/BackButton"
+import { IonIcon } from "@ionic/react"
+import { colorWandOutline } from "ionicons/icons"
+import NavButton from "../../components/NavButton"
 
 const FilterScreen = () => {
     const fileContext = useContext(FileContext)
 
     return (
-        <div>
-            
+        <div className="w-screen">
+            <div className="overflow-auto p-[0.3rem] flex flex-row justify-between">
+                <div className="">
+                    <BackButton toRoute="/home"></BackButton>
+                </div>
+                <div>
+                    <NavButton
+                            className=" dark:border-white dark:text-white dark:bg-black dark:hover:bg-gray dark:active:bg-blue
+                                        border-black text-black bg-white hover:bg-gray active:bg-blue"
+                            toRoute="/results-page"
+                    >
+                        <IonIcon
+                            icon={colorWandOutline}
+                            size="small"
+                            className="self-center"
+                        />
+                        <span>Apply Filters</span>
+                    </NavButton>
+                </div>
+            </div>
         </div>
+        
+        
         
     )
 }
