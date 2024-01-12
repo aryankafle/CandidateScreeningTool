51b90934c8233e330f217f5d34ad408148ac0ae3
chrisjo11 <68395837+chrisjo11@users.noreply.github.com>
2024-01-12 09:04:41 -0500

    reverted changes to results card list element

--- CST/src/pages/ResultsScreen/index.tsx
+++ CST/src/pages/ResultsScreen/index.tsx
@@ -69,7 +69,7 @@ const ResultsScreen = () => {
             </div>
             <div className="flex justify-center">
                 <ol className="space-y-5">
-                    {candidates.map((Candidate)=><li className="hover:bg-green" onClick={showIndividualCandidate}><ResultsCard candidate={Candidate}></ResultsCard></li>)}
+                    {candidates.map((Candidate)=><li><ResultsCard candidate={Candidate}></ResultsCard></li>)}
                 </ol>
             </div>
         </div>
