0bd9a4b966023c256e59855d64eb07d3f04f122a
aryankafle <akafl916@gmail.com>
2024-01-14 15:08:07 -0500

    fixed spacing in header for filter page

--- CST/src/components/FilterScreenHeaderButtons/index.tsx
+++ CST/src/components/FilterScreenHeaderButtons/index.tsx
@@ -11,10 +11,10 @@ const HeaderButtons = () => {
 
     return (
         <div className="overflow-auto p-[0.3rem] flex flex-row justify-between">
-            <div className="">
+            <div className="flex flex-col justify-center">
                 <BackButton toRoute="/home"></BackButton>
             </div>
-            <div>
+            <div className="flex flex-col justify-center">
                 <NavButton
                         className=" dark:border-white dark:text-white dark:bg-black dark:hover:bg-gray dark:active:bg-blue
                                     border-black text-black bg-white hover:bg-gray active:bg-blue"
