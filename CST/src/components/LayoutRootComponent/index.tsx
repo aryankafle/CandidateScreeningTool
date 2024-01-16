6d5af53aa974252fee5dea2408ebf7a8b5a0bc09
aryankafle <akafl916@gmail.com>
2024-01-16 18:05:04 -0500

    changed root styling back to clip

--- CST/src/components/LayoutRootComponent/index.tsx
+++ CST/src/components/LayoutRootComponent/index.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 const Root = (props: {children : ReactNode}) => {
   return (
-    <div className="w-screen h-screen flex flex-col overflow-auto">
+    <div className="w-screen h-screen flex flex-col overflow-clip">
        {props.children}
     </div>
   )
