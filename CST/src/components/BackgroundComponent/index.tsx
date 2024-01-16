f4c157de9f20b60614b28b2bcc4668740b8b27a5
aryankafle <akafl916@gmail.com>
2024-01-16 17:42:31 -0500

    clarified and simplified styling for background component

--- CST/src/components/BackgroundComponent/index.tsx
+++ CST/src/components/BackgroundComponent/index.tsx
@@ -1,7 +1,9 @@
 const BackgroundComponent = () => {
 
     return (
-        <div className="w-full h-[10rem] bg-blue dark:bg-green text-white dark:text-black" />
+        <div className="bg-blue dark:bg-green text-white dark:text-black
+                        absolute z-[-1] w-screen h-[9rem] "
+        />
     )
 }
 
