50b398f684a14ff870f0f801883608f9827d9bad
aryankafle <akafl916@gmail.com>
2024-01-16 17:42:05 -0500

    changed layout root to be justified auto

--- CST/src/components/LayoutRootComponent/index.tsx
+++ CST/src/components/LayoutRootComponent/index.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 const Root = (props: {children : ReactNode}) => {
   return (
-    <div className="w-screen h-screen flex flex-col justify-between overflow-auto">
+    <div className="w-screen h-screen flex flex-col overflow-auto">
        {props.children}
     </div>
   )
