959d9ebd4d1f3e629b0ebce98e085ec1cb413b24
Christopher Jo <chrisjo1105@gmail.com>
2024-01-18 01:56:33 -0500

    different content in panel depending on open or close state

--- CST/src/pages/ResultsScreen/index.tsx
+++ CST/src/pages/ResultsScreen/index.tsx
@@ -95,8 +95,16 @@ const ResultsScreen = () => {
                     </ol>
                 </div>
             </div>  
-            <div className={showSidePanel ? `flex flex-row w-2/5 h-screen bg-white justify-center` : `flex flex-row w-1/10 h-screen bg-white justify-center`} >
-                hello
+            <div className={showSidePanel ? `flex flex-row w-2/5 h-screen bg-white justify-center` : `flex flex-row w-1/10 h-screen bg-white justify-center`} onClick={()=>setShowSidePanel(!showSidePanel)}>
+                {showSidePanel ?
+                    <div>
+                        hello
+                    </div>
+                :
+                    <div>
+                        goodbye
+                    </div>
+                }
             </div>
         </div>
     );
