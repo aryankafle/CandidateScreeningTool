633c9d378c44fec28ae5bc620d630a12dcd9a091
Nathank0126753 <78155248+Nathank0126753@users.noreply.github.com>
2024-01-17 22:01:53 -0500

    adding some files

--- CST/src/router/authRouter.tsx
+++ CST/src/router/authRouter.tsx
@@ -1,9 +1,13 @@
+import { Route, Routes } from "react-router-dom"
+import ResultsScreen from "../pages/ResultsScreen"
 const authRoutes = () => {
 
     return (
-        <div>
-            auth routes
-        </div>
+        <>
+            <Routes>
+                <Route element={<ResultsScreen />}>
+            </Routes>
+        </>
     )
 }
 
