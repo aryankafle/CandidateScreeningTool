bcdb2fd78d8b4361e719ed0a34cb267fbc587688
chrisjo11 <68395837+chrisjo11@users.noreply.github.com>
2024-01-25 14:15:49 -0500

    tried to unsuccessfully fix mongodb routing stuff

--- Server/src/database/MongoDB.database.js
+++ Server/src/database/MongoDB.database.js
@@ -1,14 +1,17 @@
 import client from "../config/MongoDB.config.js"
 
-const connection = await client.connect(async (err) => {
-    if(err) {
-        console.log("MySQL: Connection error: Could not connect to database from server. The following error was thrown: ", err)
-        await client.close();
-    } else {
+const connection = async () => {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    }
-})
+      } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+      }
+}
 
 export const checkMongoDBConnection = () => {
     return !!connection
