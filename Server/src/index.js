8976f232dd3db63a2681edbe0c070a17bef916bc
chrisjo11 <68395837+chrisjo11@users.noreply.github.com>
2024-01-25 09:53:03 -0500

    MongoDB connects successfully off basic code in index

--- Server/src/index.js
+++ Server/src/index.js
@@ -6,7 +6,8 @@ import linkedInRoutes from "./routes/LinkedIn.routes.js"
 import mySQLRoutes from "./routes/MySQL.routes.js"
 import openAIRoutes from "./routes/OpenAI.routes.js"
 
-
+import { MongoClient, ServerApiVersion } from 'mongodb';
+const uri = "mongodb+srv://chrisjo1105:Snappingturtles1@cluster0.csvgwxo.mongodb.net/?retryWrites=true&w=majority";
 
 
 
@@ -21,6 +22,31 @@ app.use(express.json());
 app.use("/linkedIn", linkedInRoutes)
 app.use("/mySQL", mySQLRoutes)
 app.use("/openAI", openAIRoutes)
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+
+  async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
+
+
 
 app.get("/ping", (req, res) => {
     res.status(200).json({message: "pong"})
