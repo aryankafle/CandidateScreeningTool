871383acb5f40c863b3ce71d83860bc8cf35f06c
Christopher Jo <chrisjo1105@gmail.com>
2024-01-29 20:47:40 -0500

    refactored database to use init

--- Server/src/database/MongoDB.database.js
+++ Server/src/database/MongoDB.database.js
@@ -1,33 +1,5 @@
-import mongoConfig from "../config/MongoDB.config.js"
-import { MongoClient } from 'mongodb';
-import dotenv from "dotenv"
-dotenv.config();
-
-
-
-
-
-const client = new MongoClient(process.env.MONGODB_ACCESS_URI, mongoConfig);
-
-var isConnected = false;
-
-
-
-
-
-const connection = async () => {
-    try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        isConnected = true
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-      } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
-      }
-}
+import connection from "./init/MongoDB.init.js"
+import { isConnected } from "./init/MongoDB.init.js"
 
 connection()
 
