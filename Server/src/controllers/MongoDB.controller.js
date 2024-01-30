aa079607ddcd9a9187117fe817d85ffa9380c5f4
chrisjo11 <68395837+chrisjo11@users.noreply.github.com>
2024-01-30 09:45:07 -0500

    removed broken code

--- Server/src/controllers/MongoDB.controller.js
+++ Server/src/controllers/MongoDB.controller.js
@@ -1,18 +1,8 @@
 import { testMongoDBConnection } from "../models/services/MongoDB.service.js";
-import { connection, client, isConnected } from "../inits/MongoDB.init.js"
+
 
 export const testMongoDatabaseConnection = async (req, res) => {
     testMongoDBConnection() ? console.log("MONGODB: Connected to Database") : console.log("Error Connecting to Database")
     return testMongoDBConnection() ? res.status(200).send({message: "Connected to Database"}) : res.status(404).send({message: "Error Connecting to Database"})
 }
 
-// export const viewTable = async() => {
-//     if (isConnected) {
-//         const db = client.db("sample_mflix")
-//         const coll = db.coll("comments")
-//         const cursor = coll.find()
-//         await cursor.forEach(console.log)
-//     }
-// }
-
-// viewTable()
