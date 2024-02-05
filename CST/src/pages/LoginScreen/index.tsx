f0f5c4ef5c0c45c33bf908cc4bf9df041da22d10
Nathank0126753 <78155248+Nathank0126753@users.noreply.github.com>
2024-02-05 01:05:30 -0500

    image text can be read

--- CST/src/pages/LoginScreen/index.tsx
+++ CST/src/pages/LoginScreen/index.tsx
@@ -4,6 +4,8 @@ import  axios  from 'axios';
 import {useState } from "react";
 import querystring from 'querystring'
 import oauth from 'axios-oauth-client'
+import { createWorker } from 'tesseract.js';
+import test from './test.png'
 
 
 const clientId = process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID as string
@@ -16,36 +18,27 @@ const redirectUrl =  'http://localhost:3000/home'
 const LoginScreen = () => {
     const [account, setAccount] = useState("")
     const data = { 'grant_type': 'client_credentials'};
-    
-    const getAuthorizationCode = oauth.authorizationCode(
-        axios.create(),
-        'https://oauth.com/2.0/token', // OAuth 2.0 token endpoint
-        'CLIENT_ID',
-        'CLIENT_SECRET',
-        'https://your-app.com/oauth-redirect' // Redirect URL for your app
-      )
-      const auth = await getAuthorizationCode('AUTHORIZATION_CODE')
+    const [response, setResponse] = useState("");
+    // const getAuthorizationCode = oauth.authorizationCode(
+    //     axios.create(),
+    //     'https://oauth.com/2.0/token', // OAuth 2.0 token endpoint
+    //     'CLIENT_ID',
+    //     'CLIENT_SECRET',
+    //     'https://your-app.com/oauth-redirect' // Redirect URL for your app
+    //   )
+    //   const auth = await getAuthorizationCode('AUTHORIZATION_CODE')
 
 
 
 
     function login() {
-        
-        //Response.writeHead(301, { "Location": authorizationUrl });
-
-    //     axios.post("https://accounts.google.com/o/oauth2/v2/auth",
-    //     { 
-    //         client_id: clientId,
-            
-    //         data:({
-    //             response_type: "code",
-    //             redirect_url: redirectUrl,
-    //         })
-
-    
-    
-    // })
-      
+        (async () => {
+            const worker = await createWorker('eng');
+            const ret = await worker.recognize(test)
+            console.log(ret.data.text);
+            setResponse(ret.data.text)
+            await worker.terminate();
+          })();  
     }
     return (
         <div>
