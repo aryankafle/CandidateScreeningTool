dcc4ec968a4f60819059457337151e3f7ce9d385
Nathank0126753 <78155248+Nathank0126753@users.noreply.github.com>
2024-02-05 01:51:06 -0500

    need to mvoe some stuff to backend

--- CST/src/pages/LoginScreen/index.tsx
+++ CST/src/pages/LoginScreen/index.tsx
@@ -5,7 +5,8 @@ import {useState } from "react";
 import querystring from 'querystring'
 import oauth from 'axios-oauth-client'
 import { createWorker } from 'tesseract.js';
-import test from './test.png'
+//import test from './test.png'
+import {pdfToPng, PngPageOutput} from 'pdf-to-png-converter'
 
 
 const clientId = process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID as string
@@ -16,9 +17,12 @@ const redirectUrl =  'http://localhost:3000/home'
 
 
 const LoginScreen = () => {
-    const [account, setAccount] = useState("")
-    const data = { 'grant_type': 'client_credentials'};
+    //const img = convert(1);
+    // const [account, setAccount] = useState("")
+    // const data = { 'grant_type': 'client_credentials'};
+    
     const [response, setResponse] = useState("");
+    const [pngPage, setPngPage] = useState([] as PngPageOutput[])
     // const getAuthorizationCode = oauth.authorizationCode(
     //     axios.create(),
     //     'https://oauth.com/2.0/token', // OAuth 2.0 token endpoint
@@ -29,12 +33,29 @@ const LoginScreen = () => {
     //   const auth = await getAuthorizationCode('AUTHORIZATION_CODE')
 
 
-
-
     function login() {
+        // test(`Convert PDF To PNG`, async () => {
+        //     const pngPages: PngPageOutput[] = await pdfToPng('./Half_Day_Schedule.pdf', // The function accepts PDF file path or a Buffer
+        //     {
+        //         viewportScale: 2.0, 
+        //         pagesToProcess: [1],   
+        //     });
+        //     setPngPage(pngPages)
+        // });
+        //MOVE TO BACKEND
+
+        // const convertPdfToImg = async () => {
+        //     const pngPage = await pdfToPng('./Half_Day_Schedule.pdf', {
+        //         pagesToProcess: [1],
+        //         viewportScale: 2.0
+        // });
+        // return pngPage[0].content
+        // }
+        // setPngPage(convertPdfToImg)
+        
         (async () => {
             const worker = await createWorker('eng');
-            const ret = await worker.recognize(test)
+            const ret = await worker.recognize(pngPage[0].content)
             console.log(ret.data.text);
             setResponse(ret.data.text)
             await worker.terminate();
