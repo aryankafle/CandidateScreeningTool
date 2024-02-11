f5a18ca27206a5202249522c676672724bbfec05
Nathank0126753 <78155248+Nathank0126753@users.noreply.github.com>
2024-02-11 13:31:31 -0500

    removed some backend comments

--- Server/src/controllers/textscan.controller.js
+++ Server/src/controllers/textscan.controller.js
@@ -1,28 +1,20 @@
 import {pdfToPng} from 'pdf-to-png-converter'
 import { createWorker } from 'tesseract.js';
-//import pdf from "../../../example.pdf"
 import fs from "fs"
-//console.log(pdf)
+
 const buff =  fs.readFileSync("./example.pdf")
+
 export const convertPdfToImg = async (req, res) => {
+
     const pngPage = await pdfToPng(buff, {
         pagesToProcess: [1],
         viewportScale: 2.0,
     });
+
     console.log(pngPage[0].content)
     const worker = await createWorker('eng');
     const ret = await worker.recognize(pngPage[0].content)
-    // console.log(ret.data.text);
-    // setResponse(ret.data.text)
     await worker.terminate();
     return res.status(200).send({message:ret.data.text})
 
 }
-// export const scantext = async (req, res) => {
-//     const worker = await createWorker('eng');
-//             const ret = await worker.recognize(req.query.message)
-//             // console.log(ret.data.text);
-//             // setResponse(ret.data.text)
-//             await worker.terminate();
-//             return ret.data.text;
-// }
--- Server/src/routes/textScan.routes.js
+++ Server/src/routes/textScan.routes.js
@@ -2,12 +2,10 @@ import express from "express";
 
 import {
     convertPdfToImg ,
-    //scantext
 } from "../controllers/textscan.controller.js"
 
 const router = express.Router();
 
 router.get("/convert-pdf-to-img", convertPdfToImg)
-//router.get("/ScanText", scantext)
 
 export default router;
