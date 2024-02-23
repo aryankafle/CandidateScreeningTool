53b9812273f620e69ac3fbcfb75fc24521d67783
Christopher Jo <chrisjo1105@gmail.com>
2024-02-22 23:44:21 -0500

    removed unused controller function

--- Server/src/controllers/MongoDB.controller.js
+++ Server/src/controllers/MongoDB.controller.js
@@ -1,14 +1,9 @@
 import { testMongoDBConnection } from "../models/services/MongoDB.service.js";
 import { viewTable, insertResumeData } from "../models/services/MongoDB.service.js";
+import { convertPdfToImg } from "./textscan.controller.js";
 
 
 export const testMongoDatabaseConnection = async (req, res) => {
     testMongoDBConnection() ? console.log("MONGODB: Connected to Database") : console.log("Error Connecting to Database")
     return testMongoDBConnection() ? res.status(200).send({message: "Connected to Database"}) : res.status(404).send({message: "Error Connecting to Database"})
-}
-
-export const viewCommentsTable = async (req, res) => {
-    testMongoDBConnection() ? viewTable() : console.log("error")
-    insertResumeData('Jack', 12, 'ex')
-    return viewTable() ? res.status(200).send({message: "Printed table in console"}) : res.status(404).send({message: "Error querying data"})
 }
