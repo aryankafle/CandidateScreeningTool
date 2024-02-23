c8240c33fdc87010f664360bff5ef5fe5ebb890a
Christopher Jo <chrisjo1105@gmail.com>
2024-02-22 23:58:22 -0500

    created controller for uploading resumes to database

--- Server/src/controllers/MongoDB.controller.js
+++ Server/src/controllers/MongoDB.controller.js
@@ -7,3 +7,10 @@ export const testMongoDatabaseConnection = async (req, res) => {
     testMongoDBConnection() ? console.log("MONGODB: Connected to Database") : console.log("Error Connecting to Database")
     return testMongoDBConnection() ? res.status(200).send({message: "Connected to Database"}) : res.status(404).send({message: "Error Connecting to Database"})
 }
+
+export const uploadResumesToDB = async (req, res, listID, userToken) => {
+    if (testMongoDBConnection()) {
+        const images = convertPdfToImg(req.files)
+        insertResumeData(images, listID, userToken)
+    }
+}
