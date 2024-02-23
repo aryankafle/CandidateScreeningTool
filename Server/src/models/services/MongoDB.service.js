8eb05515095d4ab7db575e8791fa73fa1074159c
Christopher Jo <chrisjo1105@gmail.com>
2024-02-22 21:13:36 -0500

    created function to update resume entries with filters

--- Server/src/models/services/MongoDB.service.js
+++ Server/src/models/services/MongoDB.service.js
@@ -10,7 +10,7 @@ export const viewTable = async () => {
     const db = client.db("sample_mflix")
     const coll = db.collection("comments")
     const cursor = coll.find()
-    //await cursor.forEach(console.log)
+    await cursor.forEach(console.log)
 }
 
 export const insertResumeData = async (fileFormData, listID, userToken) => {
@@ -24,5 +24,21 @@ export const insertResumeData = async (fileFormData, listID, userToken) => {
             filteredResults: null
         }];
     const result = await coll.insert(doc);
-    console.log(result.insertedIds);
+    console.log("Inserted IDs: " + result.insertedIds);
+}
+
+export const updateResumeFilters = async (listID, userToken, filters) => {
+    const db = client.db("resumes")
+    const coll = db.collection("example_list")
+    const setDocs = [{
+        userToken: userToken,
+        listID: listID
+    }]
+    const updateDocs = [{
+        $set: {
+            filters: filters
+        }
+    }]
+    const result = await coll.updateMany(setDocs, updateDocs)
+    console.log("Number of updated docs: " + result.modifiedCount)
 }
