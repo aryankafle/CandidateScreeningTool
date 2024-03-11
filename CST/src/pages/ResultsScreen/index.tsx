7e08691352cec66afcb20c635ec8116c8fdd39f1
Christopher Jo <chrisjo1105@gmail.com>
2024-03-11 06:16:37 -0400

    more merge conflicts

--- CST/src/pages/ResultsScreen/index.tsx
+++ CST/src/pages/ResultsScreen/index.tsx
@@ -14,6 +14,7 @@ import { FileContext } from "../../context/FileContext";
 import { FilterContext } from "../../context/FilterContext";
 import { SavedList } from "../../utils/SavedList";
 import { UserContext } from "../../context/UserContext";
+import { FlagContext } from "../../context/FlagContext";
 
 
 const ResultsScreen = () => {
