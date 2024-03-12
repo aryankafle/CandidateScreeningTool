defab32643c65c14db8c7e0c08e2621db8e70d26
martinjang14 <77016729+martinjang14@users.noreply.github.com>
2024-03-12 00:39:16 -0400

    slight position change

--- CST/src/pages/SplashScreen/index.tsx
+++ CST/src/pages/SplashScreen/index.tsx
@@ -4,7 +4,7 @@ const SplashScreen = () => {
     const navigate = useNavigate()
 
     return (
-        <div className="flex items-center justify-center text-7xl m-20
+        <div className="text-center text-7xl m-20 p-20
                         animate-text bg-gradient-to-r from-blueDark to-blueLight bg-clip-text text-transparent">
             Candidate Screening Tool
         </div>
