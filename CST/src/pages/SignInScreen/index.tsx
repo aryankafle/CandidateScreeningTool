7d430cd4cd92b1f43ade6ad94eb9445219cb1dd3
martinjang14 <77016729+martinjang14@users.noreply.github.com>
2024-03-12 00:15:28 -0400

    padding and border

--- CST/src/pages/SignInScreen/index.tsx
+++ CST/src/pages/SignInScreen/index.tsx
@@ -37,7 +37,8 @@ const LoginScreen = () => {
     return (
         <div className="text-center text-2xl">
             <button
-                onClick = {handleLogin} className="text-grayDark hover:text-grayMid">
+                onClick = {handleLogin} className="text-black hover:text-grayMid border-2 border-spacing-8 
+                                                    rounded m-4 p-1">
                     Sign In       
             </button>
         </div>
