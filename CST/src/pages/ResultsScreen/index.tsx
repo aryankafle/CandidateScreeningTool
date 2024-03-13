f4e74f2e380a4b845cc2d8b70a4885110c65a107
martinjang14 <77016729+martinjang14@users.noreply.github.com>
2024-03-12 23:53:35 -0400

    duplicate name modal

--- CST/src/pages/ResultsScreen/index.tsx
+++ CST/src/pages/ResultsScreen/index.tsx
@@ -224,16 +224,16 @@ const ResultsScreen = () => {
                 }
                 {listWithSameName &&
                     <Modal modalTrigger={!!listWithSameName} onClose={()=>{setListWithSameName(undefined)}}>
-                        <div className="flex flex-col h-[80%] w-[60%] bg-green dark:bg-grayDark self-center">
-                            <div className="text-grayLight leading-10">
+                        <div className="flex flex-col h-[80%] w-[60%] bg-green dark:bg-grayDark self-center border-2 border-grayMid rounded">
+                            <div className="text-grayLight leading-10 text-center">
                                 You already have a saved list named {title}.
                             </div>
-                            <div className="text-grayLight leading-10"
+                            <div className="text-grayLight leading-10 hover:text-grayMid mx-10"
                                 onClick={() => setListWithSameName(undefined)}
                             >
                                 Go back
                             </div>
-                            <div className="text-blueLight leading-10"
+                            <div className="text-blueLight leading-10 hover:text-blueMid mx-10"
                                 onClick={handleReplaceListWithSameName}
                             >
                                 Replace existing list (name: {listWithSameName.name}, description: {listWithSameName.description})
@@ -289,7 +289,7 @@ const ResultsScreen = () => {
                                 <div className="flex flex-row flex-grow items-end pb-[1rem]">
                                     {!isPreviousSavedList &&
                                         <Button
-                                            className="flex flex-row gap-[1rem] bg-red dark:bg-yellow p-[0.5rem] rounded-[1rem]"
+                                            className="flex flex-row gap-[1rem] bg-red dark:bg-blueLight p-[0.5rem] rounded-[1rem]"
                                             onClick={() => { handleSaveList() }}
                                         >
                                             <div
