1b5e689962cb48ce3a552cf1df705727bd72f04f
Christopher Jo <chrisjo1105@gmail.com>
2024-03-12 23:37:31 -0400

    fixed crypto

--- Server/src/models/services/MongoDB.service.js
+++ Server/src/models/services/MongoDB.service.js
@@ -45,8 +45,7 @@ export const uploadNewSavedList = async (original_files, file_textscans, saved_l
 
 
 
-    // const filesID = crypto.randomUUID()
-    const filesID = "1235"
+    const filesID = crypto.randomUUID()
 
 
 
