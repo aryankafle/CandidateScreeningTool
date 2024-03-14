48c2edf5c359dbe5e07d8d9ef3fdd740257cb206
aryankafle <akafl916@gmail.com>
2024-03-13 21:03:36 -0400

    removed one exclamation point

--- Server/src/index.js
+++ Server/src/index.js
@@ -39,7 +39,7 @@ app.use(
         secret: "secret_session",
         resave: false,
         saveUninitialized: true,
-        cookie: { secure: !!process.env.LOCAL }
+        cookie: { secure: !process.env.LOCAL }
     })
 )
 
