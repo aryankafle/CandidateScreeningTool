d042f1651fb3ace63bf9d9edbb9513c707efc610
aryankafle <akafl916@gmail.com>
2024-03-13 22:47:53 -0400

    bru

--- Server/src/index.js
+++ Server/src/index.js
@@ -33,32 +33,26 @@ import {
 
 const app = express();
 
-app.use(cors({
-    origin: [process.env.CLIENT, "http://localhost"],
-    methods: "GET,POST,PUT,DELETE",
-    credentials: true,
-}))
-
 app.set('trust proxy', 1)
-
 app.use(
     expressSession({
         secret: "secret_session",
         resave: false,
         saveUninitialized: true,
-        cookie: { 
-            secure: !process.env.LOCAL,
-            sameSite: "none",
-            maxAge: 1000*60*60*24*7 
-        }
+        cookie: { secure: !process.env.LOCAL }
     })
 )
 
 app.use(passport.initialize())
 app.use(passport.session())
 
+app.use(cors({
+    origin: process.env.CLIENT,
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+}))
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 
 
