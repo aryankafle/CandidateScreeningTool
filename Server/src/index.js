e07a8f919580c4f42b2116fd8966088b8a2f997d
aryankafle <akafl916@gmail.com>
2024-04-03 16:07:04 -0400

    changed acao header

--- Server/src/index.js
+++ Server/src/index.js
@@ -45,12 +45,12 @@ app.use(cors({
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Credentials", true);
-    res.header("Access-Control-Allow-Origin", "https://my.godaddy.subdomain");
+    res.header("Access-Control-Allow-Origin", "https://archnatincandidatescreeningtool.netlify.app");
     res.header("Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-HTTP-Method-Override, Set-Cookie, Cookie");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     next();  
- });
+});
 
 app.use(
     expressSession({
