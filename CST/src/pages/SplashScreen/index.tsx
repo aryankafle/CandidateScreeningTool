69cd0a36d22809c2cccae05cbc1a3b966d144c15
Christopher Jo <chrisjo1105@gmail.com>
2024-05-26 03:19:08 -0400

    removed unused box

--- CST/src/pages/SplashScreen/index.tsx
+++ CST/src/pages/SplashScreen/index.tsx
@@ -19,42 +19,36 @@ const SplashScreen = () => {
                 justifyContent={"center"}
             >
 
-                <Box
-                    
+                <Typography
+                    fontSize={"5rem"}
+                    sx={{
+                        "@keyframes fontColor": {
+                            "0%": {
+                                color: palette.text.primary
+                            },
+                            "50%": {
+                                color: palette.text.secondary
+                            },
+                            "100%": {
+                                color: palette.text.primary
+                            }
+                        },
+                        "@keyframes fontSize": {
+                            "0%": {
+                                fontSize: "5rem"
+                            },
+                            "100%": {
+                                fontSize: "6rem"
+                            }
+                        },
+                        animation: "fontColor 5s ease infinite",
+                        "&:hover": {animation: "fontSize 1s ease 1 forwards"}
+                    }}
                 >
 
-                    <Typography
-                        fontSize={"5rem"}
-                        sx={{
-                            "@keyframes fontColor": {
-                                "0%": {
-                                    color: palette.text.primary
-                                },
-                                "50%": {
-                                    color: palette.text.secondary
-                                },
-                                "100%": {
-                                    color: palette.text.primary
-                                }
-                            },
-                            "@keyframes fontSize": {
-                                "0%": {
-                                    fontSize: "5rem"
-                                },
-                                "100%": {
-                                    fontSize: "6rem"
-                                }
-                            },
-                            animation: "fontColor 5s ease infinite",
-                            "&:hover": {animation: "fontSize 1s ease 1 forwards"}
-                        }}
-                    >
+                    Candidate Screening Tool
 
-                        Candidate Screening Tool
-
-                    </Typography>
-
-                </Box>
+                </Typography>
                 
             </Stack>
 
