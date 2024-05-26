4026639a36a590a73ab0d00a499e4643a43268a2
Christopher Jo <chrisjo1105@gmail.com>
2024-05-26 02:57:48 -0400

    animations

--- CST/src/pages/SplashScreen/index.tsx
+++ CST/src/pages/SplashScreen/index.tsx
@@ -1,6 +1,10 @@
-import { Stack, Typography } from "@mui/material"
+import { Palette } from "@mui/icons-material"
+import { Box, Stack, Typography, useTheme } from "@mui/material"
 
 const SplashScreen = () => {
+
+    const { palette } = useTheme()
+
     return (
 
         <Stack
@@ -15,12 +19,43 @@ const SplashScreen = () => {
                 justifyContent={"center"}
             >
 
-                <Typography
-                    fontSize={"5rem"}
+                <Box
+                    
                 >
-                    Candidate Screening Tool
-                </Typography>
 
+                    <Typography
+                        fontSize={"5rem"}
+                        sx={{
+                            "@keyframes fontColor": {
+                                "0%": {
+                                    color: palette.text.primary
+                                },
+                                "50%": {
+                                    color: palette.text.secondary
+                                },
+                                "100%": {
+                                    color: palette.text.primary
+                                }
+                            },
+                            "@keyframes fontSize": {
+                                "0%": {
+                                    fontSize: "5rem"
+                                },
+                                "100%": {
+                                    fontSize: "6rem"
+                                }
+                            },
+                            animation: "fontColor 5s ease infinite",
+                            "&:hover": {animation: "fontSize 1s ease 1 forwards"}
+                        }}
+                    >
+
+                        Candidate Screening Tool
+
+                    </Typography>
+
+                </Box>
+                
             </Stack>
 
         </Stack>
